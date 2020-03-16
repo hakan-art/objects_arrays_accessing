b968bcadceb27ec911e6ac27912ab20cb434c78b
Hakan Gezer <hakangezer@Hakans-MBP.fritz.box>
2020-03-16 14:46:51 +0100

    fisrt commit

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+let myMusic = [
+  {
+    kunstler: "The Beatles",
+    title: "Abbey Road",
+    release_jahr: 1969,
+    formate: ["LP", "CD", "MC", "Download"],
+    gold: true
+  },
+  {
+    kunstler: "Pink Floyd",
+    title: "Dark Side of the Moon",
+    release_jahr: 1978,
+    formate: ["CS", "CD", "LP", "Download"],
+    gold: true
+  },
+  {
+    kunstler: "Led Zeppelin",
+    title: "Led Zeppelin IV",
+    release_jahr: 1971,
+    formate: ["CS", "LP", "Download"],
+    gold: true
+  },
+  {
+    kunstler: "Metallica",
+    title: "Kill ’Em All und Ride the Lightning",
+    release_jahr: 1983,
+    formate: ["LP", "CD", "MC", "Download"],
+    gold: true
+  }
+];
+
+myMusic.push({
+  künstler: "Queen",
+  title: "Bohamian Rhapsody",
+  release_jahr: 1987,
+  formate: ["CS", "LP", "Download"],
+  gold: true
+});
+console.log(myMusic);
